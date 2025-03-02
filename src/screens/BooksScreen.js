00019549bc308a1818c10899859aea7c5e5bdfa2
actjacob <yakupaktar99@gmail.com>
2025-03-02 21:57:16 +0300

    feaature:fetching book info with axios  and show flatlist in bookscreen

--- src/screens/BooksScreen.js
+++ src/screens/BooksScreen.js
@@ -1,84 +1,114 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, FlatList, TouchableOpacity, StatusBar, Image } from "react-native";
+import axios from "axios";
+import colors from "../components/constants/colors";
 
-const books = [
-  {
-    id: 1,
-    title: "Sefiller",
-    author: "Victor Hugo",
-    year: 1862,
-    image: "https://example.com/sefiller.jpg",
-  },
-  {
-    id: 2,
-    title: "Suç ve Ceza",
-    author: "Fyodor Dostoyevski",
-    year: 1866,
-    image: "https://example.com/sucveceza.jpg",
-  },
-  {
-    id: 3,
-    title: "Anna Karenina",
-    author: "Lev Tolstoy",
-    year: 1878,
-    image: "https://example.com/annakarenina.jpg",
-  },
-  {
-    id: 4,
-    title: "Sefiller",
-    author: "Victor Hugo",
-    year: 1862,
-    image: "https://example.com/sefiller.jpg",
-  },
-  {
-    id: 5,
-    title: "Suç ve Ceza",
-    author: "Fyodor Dostoyevski",
-    year: 1866,
-    image: "https://example.com/sucveceza.jpg",
-  },
-  {
-    id: 6,
-    title: "Anna Karenina",
-    author: "Lev Tolstoy",
-    year: 1878,
-    image: "https://example.com/annakarenina.jpg",
-  },
-  // { id: 4, name: "Genç Bir Doktorun Anıları", price: 20 },
-  // { id: 5, name: "Martin Eden", price: 82 },
-  // { id: 6, name: "Altıncı Koğuş", price: 6 },
-  // { id: 7, name: "Genç Bir Doktorun Anıları", price: 20 },
-  // { id: 8, name: "Martin Eden", price: 82 },
-  // { id: 9, name: "Altıncı Koğuş", price: 6 },
-  // { id: 10, name: "Genç Bir Doktorun Anıları", price: 20 },
-  // { id: 11, name: "Martin Eden", price: 82 },
-  // { id: 12, name: "Altıncı Koğuş", price: 6 },
-];
+// const books = [
+//   {
+//     id: 1,
+//     title: "Sefiller",
+//     author: "Victor Hugo",
+//     year: 1862,
+//     image: "https://example.com/sefiller.jpg",
+//   },
+//   {
+//     id: 2,
+//     title: "Suç ve Ceza",
+//     author: "Fyodor Dostoyevski",
+//     year: 1866,
+//     image: "https://example.com/sucveceza.jpg",
+//   },
+//   {
+//     id: 3,
+//     title: "Anna Karenina",
+//     author: "Lev Tolstoy",
+//     year: 1878,
+//     image: "https://example.com/annakarenina.jpg",
+//   },
+//   {
+//     id: 4,
+//     title: "Sefiller",
+//     author: "Victor Hugo",
+//     year: 1862,
+//     image: "https://example.com/sefiller.jpg",
+//   },
+//   {
+//     id: 5,
+//     title: "Suç ve Ceza",
+//     author: "Fyodor Dostoyevski",
+//     year: 1866,
+//     image: "https://example.com/sucveceza.jpg",
+//   },
+//   {
+//     id: 6,
+//     title: "Anna Karenina",
+//     author: "Lev Tolstoy",
+//     year: 1878,
+//     image: "https://example.com/annakarenina.jpg",
+//   },
+//   // { id: 4, name: "Genç Bir Doktorun Anıları", price: 20 },
+//   // { id: 5, name: "Martin Eden", price: 82 },
+//   // { id: 6, name: "Altıncı Koğuş", price: 6 },
+//   // { id: 7, name: "Genç Bir Doktorun Anıları", price: 20 },
+//   // { id: 8, name: "Martin Eden", price: 82 },
+//   // { id: 9, name: "Altıncı Koğuş", price: 6 },
+//   // { id: 10, name: "Genç Bir Doktorun Anıları", price: 20 },
+//   // { id: 11, name: "Martin Eden", price: 82 },
+//   // { id: 12, name: "Altıncı Koğuş", price: 6 },
+// ];
 
 const numColumns = 2;
 
 const BooksScreen = (props) => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await axios.get("http://172.20.10.3:3000/scrape", {
+          params: {
+            url: "https://www.xn--muratyklmaz-4zbb.com.tr/modern-klasikler-dizisi-listesi/",
+          },
+        });
+        console.log(response.data);
+        setBooks(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
+
+    fetchBooks();
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
         <Text style={styles.headerText}>Modern Klasikler</Text>
       </View>
-      <FlatList
-        data={books}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View style={styles.arrayContainer}>
-            <View style={styles.booksBox}>
-              <Text style={styles.bookName}> {item.title} </Text>
-              <Image />
-              <Text> {item.author} </Text>
-              <Text> {item.year} </Text>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <FlatList
+          data={books}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.arrayContainer}>
+              <View style={styles.booksBox}>
+                <Text> {item.number} </Text>
+                <Image />
+                <Text style={styles.bookName}> {item.title} </Text>
+                <Text style={styles.bookAuthor}> {item.author} </Text>
+                {/* <Text> {item.isbn} </Text> */}
+              </View>
             </View>
-          </View>
-        )}
-        numColumns={numColumns}
-      />
+          )}
+          numColumns={numColumns}
+        />
+      )}
     </View>
   );
 };
@@ -88,7 +118,7 @@ export default BooksScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
+    backgroundColor: "white",
     // alignItems: "center",
     // justifyContent: "center",
   },
@@ -102,19 +132,35 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginTop: 25,
     fontSize: 24,
-    color: "white",
+    color: colors.white,
   },
   arrayContainer: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: colors.lightGray,
     alignItems: "center",
     justifyContent: "space-between",
-    height: "250",
-    marginRight: 10,
-    margin: 2,
+    // height: "250",
+    margin: 5,
   },
   booksBox: {
-    borderRadius: 20,
+    width: 150,
+    height: 250,
+    borderRadius: 10,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
-  bookName: {},
+  bookName: {
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 5,
+  },
+  bookAuthor: {
+    fontStyle: "italic",
+    textAlign: "center",
+    marginBottom: 5,
+  },
 });
--- web-scraping-api/server.js
+++ web-scraping-api/server.js
@@ -1,64 +1,74 @@
-// const express = require("express");
-// const axios = require("axios");
-// const cheerio = require("cheerio");
+// import express from "express";
+// import axios from "axios";
+// // const cheerio = require("cheerio");
+// import * as cheerio from "cheerio"; // `import * as cheerio` ile tüm modülü içe aktar,
+// import { faker } from "@faker-js/faker";
 
-// // Express uygulamasını başlat
 // const app = express();
-// const port = 3000; // API'nin çalışacağı port
+// const port = 3000;
 
-// // Web scraping işlemi için route
 // app.get("/scrape", async (req, res) => {
 //   try {
-//     // Verilen URL'yi axios ile çekiyoruz
+//     // Dinamik bir User-Agent oluşturuyoruz
+//     const userAgent = faker.internet.userAgent();
+//     // Web sayfasını çekiyoruz
 //     const { data } = await axios.get(
-//       "https://www.xn--muratyklmaz-4zbb.com.tr/modern-klasikler-dizisi-listesi/"
+//       "https://www.xn--muratyklmaz-4zbb.com.tr/modern-klasikler-dizisi-listesi/",
+//       {
+//         // headers: {
+//         //   "User-Agent":
+//         //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+//         //   Accept: "application/json, text/plain, */*",
+//         //   "Accept-Encoding": "gzip, compress, deflate, br",
+//         //   Connection: "keep-alive",
+//         // },
+//         headers: {
+//           "User-Agent": userAgent,
+//           Accept: "application/json, text/plain, */*",
+//           "Accept-Encoding": "gzip, compress, deflate, br",
+//           Connection: "keep-alive",
+//         },
+//       }
 //     );
 
-//     // Cheerio ile HTML içeriğini parse ediyoruz
+//     // Sayfa içeriğini parse ediyoruz
 //     const $ = cheerio.load(data);
 
-//     // Kitap bilgilerini tutacak bir dizi oluşturuyoruz
+//     // Kitapları çekiyoruz
 //     const books = [];
-
-//     // Listeyi seçiyoruz ve her bir satırdaki bilgiyi çekiyoruz
-//     $("table tr").each((index, element) => {
-//       // İlk satır başlık olduğu için onu atlıyoruz
-//       if (index === 0) return;
-
-//       // Kitap bilgilerini çekiyoruz
+//     $("table tbody tr").each(async (index, element) => {
 //       const bookNumber = $(element).find("td:nth-child(1)").text().trim();
-//       const isbn = $(element).find("td:nth-child(2)").text().trim();
-//       const bookName = $(element).find("td:nth-child(3)").text().trim();
-//       const author = $(element).find("td:nth-child(4)").text().trim();
+//       const bookISBN = $(element).find("td:nth-child(2)").text().trim();
+//       const bookTitle = $(element).find("td:nth-child(3)").text().trim();
+//       const bookAuthor = $(element).find("td:nth-child(4)").text().trim();
 
-//       // Kitap bilgisini diziye ekliyoruz
-//       if (bookNumber && bookName && author) {
-//         books.push({
-//           bookNumber,
-//           isbn,
-//           bookName,
-//           author,
-//         });
-//       }
+//       // const bookCover = await getBookCoverByISBN(bookISBN);
+
+//       books.push({
+//         // number: bookNumber,
+//         isbn: bookISBN,
+//         // title: bookTitle,
+//         // author: bookAuthor,
+//       });
 //     });
 
-//     // Sonuçları JSON olarak döndürüyoruz
+//     // JSON formatında sonucu döndürüyoruz
 //     res.json(books);
 //   } catch (error) {
-//     console.error("Error during scraping:", error);
-//     res.status(500).json({ error: "Web scraping failed" });
+//     console.error(error);
+//     res.status(500).send("Error occurred while scraping data.");
 //   }
 // });
 
-// // Uygulama dinlemeye başlasın
-// app.listen(port, () => {
-//   console.log(`Web scraping API listening at http://localhost:${port}`);
+// // API'nin çalışmaya başlaması
+// app.listen(port, "172.20.10.3", () => {
+//   // 0.0.0.0 kullanarak her yerel ağdan bağlantı kabul edilebilir.
+//   console.log(`Scraping API is running at http://172.20.10.3:${port}`);
 // });
 
 import express from "express";
 import axios from "axios";
-// const cheerio = require("cheerio");
-import * as cheerio from "cheerio"; // `import * as cheerio` ile tüm modülü içe aktar,
+import * as cheerio from "cheerio"; // `import * as cheerio` ile tüm modülü içe aktar
 import { faker } from "@faker-js/faker";
 
 const app = express();
@@ -68,17 +78,11 @@ app.get("/scrape", async (req, res) => {
   try {
     // Dinamik bir User-Agent oluşturuyoruz
     const userAgent = faker.internet.userAgent();
+
     // Web sayfasını çekiyoruz
     const { data } = await axios.get(
-      "https://www.xn--muratyklmaz-4zbb.com.tr/modern-klasikler-dizisi-listesi/",
+      "https://www.iskultur.com.tr/kitap/modern-klasikler?srsltid=AfmBOoqT5TvM_lYaloWTiGj50L2D36HGlgt-PzpGY9Q_8VjGDn_ehOw5#group=group1&page=0-24",
       {
-        // headers: {
-        //   "User-Agent":
-        //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-        //   Accept: "application/json, text/plain, */*",
-        //   "Accept-Encoding": "gzip, compress, deflate, br",
-        //   Connection: "keep-alive",
-        // },
         headers: {
           "User-Agent": userAgent,
           Accept: "application/json, text/plain, */*",
@@ -93,22 +97,30 @@ app.get("/scrape", async (req, res) => {
 
     // Kitapları çekiyoruz
     const books = [];
-    $("table tbody tr").each((index, element) => {
-      const bookNumber = $(element).find("td:nth-child(1)").text().trim();
-      const bookISBN = $(element).find("td:nth-child(2)").text().trim();
-      const bookTitle = $(element).find("td:nth-child(3)").text().trim();
-      const bookAuthor = $(element).find("td:nth-child(4)").text().trim();
 
-      books.push({
-        number: bookNumber,
-        isbn: bookISBN,
-        title: bookTitle,
-        author: bookAuthor,
-      });
-    });
+    // Sayfada kitap bilgilerini çekiyoruz
+    const bookPromises = $("ul#products-list li")
+      .map(async (index, element) => {
+        const bookTitle = $(element).find(".product-title").text().trim();
+        const bookAuthor = $(element).find(".product-author").text().trim();
+        const bookISBN = $(element).find(".product-isbn").text().trim();
+        const bookPrice = $(element).find(".product-price").text().trim();
+
+        // Kitap bilgilerini diziye ekliyoruz
+        return {
+          title: bookTitle,
+          author: bookAuthor,
+          isbn: bookISBN,
+          price: bookPrice,
+        };
+      })
+      .get(); // `map` metodunun dönüşünü array'e çeviriyoruz
+
+    // Asenkron işlemleri bekliyoruz
+    const booksData = await Promise.all(bookPromises);
 
     // JSON formatında sonucu döndürüyoruz
-    res.json(books);
+    res.json(booksData);
   } catch (error) {
     console.error(error);
     res.status(500).send("Error occurred while scraping data.");
@@ -116,6 +128,109 @@ app.get("/scrape", async (req, res) => {
 });
 
 // API'nin çalışmaya başlaması
-app.listen(port, () => {
-  console.log(`Scraping API is running at http://localhost:${port}`);
+app.listen(port, "172.20.10.3", () => {
+  console.log(`Scraping API is running at http://172.20.10.3:${port}`);
 });
+
+// import express from "express";
+// import axios from "axios";
+// import * as cheerio from "cheerio"; // `import * as cheerio` ile tüm modülü içe aktar
+// import fs from "fs";
+// import path from "path";
+// import https from "https";
+// import { faker } from "@faker-js/faker";
+
+// const app = express();
+// const port = 3000;
+
+// // Google Books API'yi kullanarak kitap kapağını alacak fonksiyon
+// async function getBookCoverByISBN(isbn) {
+//   try {
+//     const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`);
+//     const bookData = response.data.items[0]; // İlk kitap verisini alıyoruz
+
+//     if (bookData && bookData.volumeInfo && bookData.volumeInfo.imageLinks) {
+//       const imageUrl = bookData.volumeInfo.imageLinks.thumbnail;
+//       return imageUrl;
+//     } else {
+//       return null; // Kapağa ulaşılamazsa null döner
+//     }
+//   } catch (error) {
+//     console.error(`Error fetching book cover for ISBN ${isbn}:`, error);
+//     return null;
+//   }
+// }
+
+// // Resmi cihazınıza kaydedecek fonksiyon
+// function downloadImage(url, savePath) {
+//   https
+//     .get(url, (response) => {
+//       const fileStream = fs.createWriteStream(savePath);
+//       response.pipe(fileStream);
+
+//       fileStream.on("finish", () => {
+//         console.log(`Image saved to ${savePath}`);
+//       });
+//     })
+//     .on("error", (err) => {
+//       console.error("Error downloading image:", err);
+//     });
+// }
+
+// app.get("/scrape", async (req, res) => {
+//   try {
+//     // Dinamik bir User-Agent oluşturuyoruz
+//     const userAgent = faker.internet.userAgent();
+//     // Web sayfasını çekiyoruz
+//     const { data } = await axios.get(
+//       "https://www.xn--muratyklmaz-4zbb.com.tr/modern-klasikler-dizisi-listesi/",
+//       {
+//         headers: {
+//           "User-Agent": userAgent,
+//           Accept: "application/json, text/plain, */*",
+//           "Accept-Encoding": "gzip, compress, deflate, br",
+//           Connection: "keep-alive",
+//         },
+//       }
+//     );
+
+//     // Sayfa içeriğini parse ediyoruz
+//     const $ = cheerio.load(data);
+
+//     // Kitapları çekiyoruz
+//     const books = [];
+//     $("table tbody tr").each(async (index, element) => {
+//       const bookNumber = $(element).find("td:nth-child(1)").text().trim();
+//       const bookISBN = $(element).find("td:nth-child(2)").text().trim();
+//       const bookTitle = $(element).find("td:nth-child(3)").text().trim();
+//       const bookAuthor = $(element).find("td:nth-child(4)").text().trim();
+
+//       const bookCoverUrl = await getBookCoverByISBN(bookISBN);
+//       if (bookCoverUrl) {
+//         const fileName = `${bookISBN}.jpg`; // Resim için dosya adı (ISBN numarasını kullanarak)
+//         const savePath = path.join(__dirname, "assets", "covers", fileName); // Resmi kaydedeceğimiz yol
+
+//         // Eğer cover url varsa resmi indir
+//         downloadImage(bookCoverUrl, savePath);
+//       }
+
+//       books.push({
+//         number: bookNumber,
+//         isbn: bookISBN,
+//         title: bookTitle,
+//         author: bookAuthor,
+//       });
+//     });
+
+//     // JSON formatında sonucu döndürüyoruz
+//     res.json(books);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Error occurred while scraping data.");
+//   }
+// });
+
+// // API'nin çalışmaya başlaması
+// app.listen(port, "172.20.10.3", () => {
+//   console.log(`Scraping API is running at http://172.20.10.3:${port}`);
+// });
